9bfc2b0ec88a38f77fe83ce52852778be170c603
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2023-03-28 20:23:45 -0300

    xD

--- models/phone.js
+++ models/phone.js
@@ -20,7 +20,7 @@ mongoose.connect(url)
   })
 
 
-phoneSchema.set('toJSON', {
+  personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
       returnedObject.id = returnedObject._id.toString()
       delete returnedObject._id
