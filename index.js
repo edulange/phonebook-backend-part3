cc48e16f1307dbfbaba75c5506fac987d6d5a66e
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2023-04-04 21:00:14 -0300

    maybe?

--- index.js
+++ index.js
@@ -1,127 +1,87 @@
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+
 const app = express();
-require("dotenv").config();
-const cors = require("cors");
-const bodyParser = require("body-parser");
-app.use(cors());
-const logger = require("morgan");
+const port = process.env.PORT || 3001;
 
-const Phone = require("./models/phone");
+const Person = require('./models/person');
 
+const errorHandler = (error, _request, response, next) => {
+  if (error.name === 'CastError')
+    return response.status(400).send({ error: 'malformed id' });
+  else if (error.name === 'ValidationError')
+    return response.status(400).json({ error: error.message });
 
-app.use(bodyParser.json());
-logger.token("body", (req, res) => {
-	return JSON.stringify(req.body);
-});
+  console.error(error.message);
 
+  next(error);
+};
+
+morgan.token('body', (request) =>
+  request.method === 'POST' ? JSON.stringify(request.body) : null
+);
+
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+);
 
 app.use(express.json());
-app.use(express.static("build"));
 
+app.use(express.static('public'));
 
+app.get('/info', (_request, response, next) =>
+  Person.countDocuments()
+    .then((count) =>
+      response.send(
+        `The phonebook contains records of ${count} people.<br>${new Date()}`
+      )
+    )
+    .catch((error) => next(error))
+);
 
-app.get("/api/persons", (req, res, next) => {
-	Phone.find({})
-		.then((response) => {
-			res.json(response);
-		})
-		.catch((error) => next(error));
-});
-  
-app.get("/api/persons/:id", (req, res, next) => {
-	Phone.findById(req.params.id)
-		.then((response) => {
-			if (response) {
-				res.json(response);
-			} else {
-				res.status(404).end();
-			}
-		})
-		.catch((error) => next(error));
-});
+app.post('/api/persons', async (request, response, next) => {
+  const { name, number } = request.body;
 
-app.put("/api/persons/:id", (req, res, next) => {
-	Phone.findByIdAndUpdate(req.params.id, { number: req.body.number })
-		.then(() => {
-			Phone.findById(req.params.id)
-				.then((response) => res.json(response))
-				.catch((error) => next(error));
-		})
-		.catch((error) => next(error));
+  if (await Person.exists({ name }))
+    return response.status(400).json({ error: 'name must be unique' });
+
+  new Person({ name, number })
+    .save()
+    .then((data) => response.json(data))
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (req, res, next) => {
-	Phone.deleteOne({ _id: req.params.id })
-		.then((response) => {
-			console.log(response);
-			res.status(204).end();
-		})
-		.catch((error) => next(error));
+app.get('/api/persons', (_request, response) =>
+  Person.find().then((data) => response.json(data))
+);
+
+app.get('/api/persons/:id', (request, response, next) =>
+  Person.findById(request.params.id)
+    .then((person) =>
+      person ? response.json(person) : response.status(404).end()
+    )
+    .catch((error) => next(error))
+);
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body;
+
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true }
+  )
+    .then((person) => response.json(person))
+    .catch((error) => next(error));
 });
 
-app.post("/api/persons", (req, res, next) => {
-	if (!req.body.name || !req.body.number) {
-		res.status(400).json({
-			error: "Name and/or Number is missing",
-		});
-	} else {
-		const person = new Phone({
-			name: req.body.name,
-			number: req.body.number,
-		});
-
-		person
-			.save()
-			.then((savedPhone) => {
-				res.json(savedPhone);
-			})
-			.catch((error) => next(error));
-	}
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(() => response.status(204).end())
+    .catch((error) => next(error));
 });
 
-app.get("/info", (req, res, next) => {
-	Phone.find({})
-		.then((response) => {
-			const date = new Date(Date.now());
-			res.send(
-				`Phonebook has info for ${
-					response.length
-				} people <br/> ${date.toString()}`
-			);
-		})
-		.catch((error) => next(error));
-});
+app.use(errorHandler);
 
-
-
-  
-  const unknownEndpoint = (request, response) => {
-	response.status(404).send({ error: 'unknown endpoint' })
-  }
-  
-  // handler of requests with unknown endpoint
-  app.use(unknownEndpoint)
-  
-  const errorHandler = (error, request, response, next) => {
-	console.error(error.message)
-  
-	if (error.name === 'CastError') {
-	  return response.status(400).send({ error: 'malformatted id' })
-	} else if(error.name === 'ValidationError'){
-	  return response.status(400).json({ error: error.message })
-	}
-  
-	next(error)
-  }
-  
-  // handler of requests with result to errors
-  app.use(errorHandler)
-
-const PORT = 3001;
-app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
-});
-
-//não fazz sentido eu preciso fazer um post e um put? ou é um post ou é um put
-//a vida é só isso?
-//acho que loucura é eu achar que iss ovai mudar alguma coisa.
+app.listen(port, () => console.log(`Server listening on port ${port}.`));
